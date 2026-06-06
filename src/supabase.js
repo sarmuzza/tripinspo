206f132e1f1402e58b6d3513fb21e2fda9465f4d
sarmuzza <sarah.murray.94@hotmail.com>
2026-06-06 20:40:50 +1000

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,16 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-  },
-  global: {
-    headers: {
-      'apikey': supabaseKey,
-    }
-  }
-})
+export const supabase = createClient(
+  'https://prvnbtjjjuejjvzoffmx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBydm5idGpqanVlamp2em9mZm14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NDQ5MzksImV4cCI6MjA5NjEyMDkzOX0.GJe4cX9KcC2FaHm_KnoX_hvJzQ06FZtoD300HuBC7ms'
+)
